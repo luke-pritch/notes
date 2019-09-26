f0754feff92e651f3543a8bdfb98f5962f962e91
Luke Pritchard <luke.pritchard@protonmail.com>
2019-09-26 17:28:56 -0400

    Feature: Adding new notes and more event handling

--- src/App.js
+++ src/App.js
@@ -2,12 +2,35 @@ import React, { useState } from "react";
 import Note from "./components/Note";
 
 const App = props => {
+    const [notes, setNotes] = useState(props.notes);
+    const [newNote, setNewNote] = useState("a new note...");
     const rows = () => notes.map(note => <Note key={note.id} note={note} />);
+
+    // Event Handlers
+    const addNote = event => {
+        event.preventDefault();
+        const noteObject = { // Create new Note Object
+            content: newNote,
+            date: new Date().toISOString,
+            important: Math.random() > 0.5,
+            id: notes.length + 1
+        }
+        setNotes(notes.concat(noteObject))
+        setNewNote('') // Change input field back to blank
+    };
+    const handleNoteChange = event => {
+        console.log(event.target.value);
+        setNewNote(event.target.value);
+    };
 
     return (
         <div>
             <h1>Notes</h1>
             <ul>{rows()}</ul>
+            <form onSubmit={addNote}>
+                <input value={newNote} onChange={handleNoteChange} />
+                <button type="submit">save</button>
+            </form>
         </div>
     );
 };
